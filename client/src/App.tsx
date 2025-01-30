a8697109e23712e3b8f60ed186156339b53c5e6e
jottiana <11.paradise.avenue@gmail.com>
2025-01-30 17:09:54 +0100

    Added home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 
@@ -6,6 +7,9 @@ function App() {
   return (
     <div className="app-container">
       <Navbar />
+      <main className="main-content">
+        <Outlet />
+      </main>
       <Footer />
     </div>
   );
